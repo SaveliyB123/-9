0081b7bbe9ed651c4ff9eee0d71e5ed972b6e77e
SaveliyB123 <ek24-s.balabanov@nubip.edu.ua>
2025-05-21 17:31:07 +0200

    db.php

--- _db.php
+++ _db.php
@@ -2,8 +2,8 @@
 $host = "127.0.0.1";
 $port = 3306;
 $username = "root";
-$password = ""; // або твій пароль, якщо є!
-$database = "project_reservation"; // твоя база
+$password = ""; 
+$database = "project_reservation";
 
 $db = new PDO("mysql:host=$host;port=$port", $username, $password);
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
