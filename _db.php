8470b264d96c5f7a13bd6fe7a1d963272e640f71
SaveliyB123 <ek24-s.balabanov@nubip.edu.ua>
2025-05-21 22:24:20 +0200

    db.php

--- _db.php
+++ _db.php
@@ -1,14 +1,11 @@
 <?php
-$host = "postgresql://render_postgres_host_user:nAsi7wc6sMC1F2F3uk5QNJg9BQqzpyCl@dpg-d0n2gl95pdvs7387t2rg-a/render_postgres_host";
-$port = 5432;
-$username = "render_postgres_host_user";
-$password = "nAsi7wc6sMC1F2F3uk5QNJg9BQqzpyCl";
-$database = "render_postgres_host";
+$host = "127.0.0.1";
+$port = 3306;
+$username = "root";
+$password = "";
+$database = "project_reservation";
 
-try {
-    $db = new PDO("pgsql:host=$host;port=$port;dbname=$database", $username, $password);
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    die("Database connection failed: " . $e->getMessage());
-}
+$db = new PDO("mysql:host=$host;port=$port", $username, $password);
+$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$db->exec("USE `$database`");
 ?>
